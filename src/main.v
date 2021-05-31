5ff8f973f96f619a58720a561e57701231cabe23
Milo <50248166+Milo123459@users.noreply.github.com>
2021-05-31 13:10:50 +0100

    feat: main: minor tweaks

--- src/main.v
+++ src/main.v
@@ -7,23 +7,8 @@ mut:
 
 fn event(e &tui.Event, x voidptr) {
 	mut app := &App(x)
-	app.tui.clear()
 	app.tui.set_cursor_position(0, 0)
-	app.tui.write('V term.input event viewer (press `esc` to exit)\n\n')
-	app.tui.write('$e')
-	app.tui.write('\n\nRaw event bytes: "$e.utf8.bytes().hex()" = $e.utf8.bytes()')
-	if !e.modifiers.is_empty() {
-		app.tui.write('\nModifiers: $e.modifiers = ')
-		if e.modifiers.has(.ctrl) {
-			app.tui.write('ctrl. ')
-		}
-		if e.modifiers.has(.shift) {
-			app.tui.write('shift ')
-		}
-		if e.modifiers.has(.alt) {
-			app.tui.write('alt. ')
-		}
-	}
+
 	app.tui.flush()
 
 	if e.typ == .key_down && e.code == .escape {
@@ -37,11 +22,12 @@ fn main() {
 		user_data: app
 		event_fn: event
 		window_title: 'Vext'
-		hide_cursor: true
+		hide_cursor: false
 		capture_events: true
 		frame_rate: 60
 		use_alternate_buffer: false
 	)
-	println('V event viewer, the start of Vext\n\n')
+	app.tui.clear()
+	
 	app.tui.run() ?
 }
